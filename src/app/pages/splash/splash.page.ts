e9a9b18c6f076217c8faabb0b2cebf4bf81a892d
Json Alzate <developer.alzate@gmail.com>
2025-04-02 19:19:35 -0500

    ✨ Show progress sync

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, Signal } from '@angular/core';
+import { Component, OnInit, signal, Signal, computed, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonImg  } from '@ionic/angular/standalone';
 
+import { sync } from '@models/sync.model';
+
 import { UiService} from '@services/ui.service';
+
+
 
 @Component({
   selector: 'app-splash',
@@ -15,17 +19,70 @@ import { UiService} from '@services/ui.service';
 export class SplashPage implements OnInit {
 
   theme!: Signal<string>;
+
+  syncSteps = signal<sync[]>([
+    { id: 1, name: 'theme', sync: false, description: 'Synchronizing themes...', failed: false },
+    { id: 2, name: 'Puzzles', sync: false, description: 'Synchronizing puzzles...', failed: false },
+    { id: 3, name: 'Positions', sync: false, description: 'Synchronizing positions...', failed: false },
+    { id: 4, name: 'Coordinates', sync: false, description: 'Synchronizing coordinates...', failed: false },
+    { id: 5, name: 'Maps', sync: false, description: 'Synchronizing maps...', failed: false },
+    { id: 6, name: 'Settings', sync: false, description: 'Synchronizing settings...', failed: false }
+  ]);
+
+  percentageSync = computed(() => {
+    const completedSteps = this.syncSteps().filter(step => step.sync).length;
+    return (completedSteps / this.syncSteps().length) * 100;
+  });
+
+  currentSyncStepDescription = computed(() => {
+    const nextStep = this.syncSteps().find(step => !step.sync);
+    return nextStep ? nextStep.description : 'Synchronization complete!';
+  });
+
+  syncFailed = computed(() => this.syncSteps().some(step => step.failed));
+
   constructor(
     private uiService: UiService
   ) { 
     this.theme = this.uiService.getTheme();
+    effect(() => {
+      if (this.currentSyncStepDescription() === 'Synchronization complete!') {
+        this.onSyncComplete();
+      }
+    });
+
+    effect(() => {
+      if (this.syncFailed()) {
+        this.onSyncFailed();
+      }
+    });
   }
+
+  
 
   ngOnInit() {
+    this.startSync();
   }
 
-  changeTheme() {
-    this.uiService.setTheme('valentine');
+
+  startSync() {
+    for (let i = 0; i < this.syncSteps().length; i++) {
+      setTimeout(() => {
+        this.syncSteps.update(steps => {
+          const updatedSteps = [...steps];
+          updatedSteps[i] = { ...updatedSteps[i], sync: true }; // Use clone the object for reactivity
+          return updatedSteps;
+        });
+      }, i * 1000);
+    }
+  }
+
+  onSyncComplete() {
+    console.log('Sync complete');
+  }
+
+  onSyncFailed() {
+    console.error('Sync failed');
   }
 
 }
