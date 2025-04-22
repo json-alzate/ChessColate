dca7c8faf1c21751c2bcbafc9bc06e41582bc96a
Json Alzate <developer.alzate@gmail.com>
2025-04-22 18:45:45 -0500

    🏗️ cards-block layout

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,18 @@
-import { Component, Signal } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { IonContent, IonIcon, MenuController, IonFab, IonFabButton } from '@ionic/angular/standalone';
 
 import { UiService} from '@services/ui.service';
 
 // import { ChesscolateLibComponent } from 'chesscolate-lib';
 import { HeaderSliderComponent } from '@pages/home/widgets/header-slider/header-slider.component';
+import { CardsBlockComponent } from '@pages/home/widgets/cards-block/cards-block.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonFabButton, IonFab, IonIcon, IonContent, HeaderSliderComponent,],
+  imports: [IonFabButton, IonFab, IonIcon, IonContent, 
+    HeaderSliderComponent, CardsBlockComponent],
 })
 export class HomePage {
 
--- src/app/pages/home/widgets/cards-block/cards-block.component.ts
+++ src/app/pages/home/widgets/cards-block/cards-block.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, signal, computed  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Item {
+  id: number;
+  prioridad?: number;
+}
 
 @Component({
   selector: 'app-cards-block',
   templateUrl: './cards-block.component.html',
   styleUrls: ['./cards-block.component.scss'],
-  standalone: true,
+  imports: [CommonModule],
+  standalone: true
 })
 export class CardsBlockComponent  implements OnInit {
+
+  // @Input({ required: true })
+  // itemsInput = signal<Item[]>([]);
+
+  // @Input()
+  // layoutClass = signal<'layout1' | 'layout2' | 'layout3'>('layout1');
+
+  // @Output()
+  // itemClicked = new EventEmitter<Item>();
+
+  // readonly layoutCapacidad = 8;
+
+  // itemsVisibles = computed(() => {
+  //   const items = this.itemsInput();
+  //   const conPrioridad = items
+  //     .filter(item => item.prioridad !== undefined)
+  //     .sort((a, b) => (b.prioridad! - a.prioridad!));
+
+  //   const sinPrioridad = items
+  //     .filter(item => item.prioridad === undefined)
+  //     .sort(() => Math.random() - 0.5);
+
+  //   return [...conPrioridad, ...sinPrioridad].slice(0, this.layoutCapacidad);
+  // });
 
   constructor() { }
 
   ngOnInit() {}
+
+  seleccionar(item: Item) {
+    // this.itemClicked.emit(item);
+  }
 
 }
